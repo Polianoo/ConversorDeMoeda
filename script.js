7b1c0118df299bc664c5c348cbb163974e8ec72d
Polianoo <polianorodrigues944@outlook.com>
2024-07-04 20:45:45 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const convertButton = document.querySelector(".convert-button")
+console.log(convertButton)
+
+
+
+function convertValues() {
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+
+    /*  console.log(inputCurrencyValue) */
+
+    const dolarToday = 5.2
+    const convertedValue = inputCurrencyValue / dolarToday
+    console.log(convertedValue)
+
+}
+
+
+
+
+
+
+
